58f1d43de563e2ef8782b01025adff1b97e71c36
Caiyi Hsu <cai1hsu@outlook.com>
2023-10-10 14:58:16 +0000

    Finish `if`, `quiz1` and `primitive_types`

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+const price:i32 = 2;
+fn calculate_price_of_apples(quantity:i32) -> i32 {
+   if (quantity > 40){
+        quantity
+    }
+    else {
+        quantity * price
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
